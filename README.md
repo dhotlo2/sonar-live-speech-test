a946ed2378dd760640d833b2ee2ed983dba909e3
dhotlo2 <damirrrrrrr@aol.com>
2020-10-02 22:43:21 -0500

    more keywords and phrases

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # sonar-live-speech-test
  
- live on <a href="https://dhotlo2.github.io/sonar-live-speech-test/"> this page</a>
+live on <a href="https://dhotlo2.github.io/sonar-live-speech-test/"> this page</a>
+
+Keywords currently connected with suggestions (say one of these):
+- use cases
+- Salesforce integration
+- compliance
+
+Phrases that currently lead to commands (say one of these):
+- open use cases page
+- open Salesforce integration page
+- open compliance page
--- ai.js
+++ ai.js
@@ -1,25 +1,50 @@
 window.setInterval(function(){
   console.log("AI Speech function called");
-	 if (
+	if (
 		document.getElementById('result').innerHTML.indexOf('use cases') != -1){
-	  		document.getElementById('usecases').style.display = 'block';	
+  		document.getElementById('usecases').style.display = 'block';	
 	}
 	if (
+		document.getElementById('result').innerHTML.indexOf('open use-cases page') != -1){
+  		openU();	
+	}
+	if (
+		document.getElementById('result').innerHTML.indexOf('open use cases page') != -1){
+  		openU();	
+	}
+
+	if (
 		document.getElementById('result').innerHTML.indexOf('Salesforce integration') != -1){
-	  		document.getElementById('salesforce').style.display = 'block';	
+  		document.getElementById('salesforce').style.display = 'block';	
+	}
+	if (
+		document.getElementById('result').innerHTML.indexOf('open Salesforce integration page') != -1){
+  		openS();
+	}
+
+	if (
+		document.getElementById('result').innerHTML.indexOf('compliance') != -1){
+  		document.getElementById('compliance').style.display = 'block';	
+	}
+	if (
+		document.getElementById('result').innerHTML.indexOf('open compliance page') != -1){
+  		openC();
 	}
 }, 500);
 
 function closeUsecases() {
-	console.log("test works");
 	document.getElementById("usecases").style.position = 'absolute';
 	document.getElementById("usecases").style.right = '-500px';
 }
 
 function closeSalesforce() {
-	console.log("test works");
 	document.getElementById("salesforce").style.position = 'absolute';
 	document.getElementById("salesforce").style.right = '-500px';
+}
+
+function closeCompliance() {
+	document.getElementById("compliance").style.position = 'absolute';
+	document.getElementById("compliance").style.right = '-500px';
 }
 
 function test() {
@@ -34,3 +59,36 @@ function showLive() {
 	document.getElementById("livecall").style.display = 'block';
 }
 
+
+var openU = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            var win = window.open('https://www.sendsonar.com/use-cases.html', '_blank');
+			 win.focus();
+        }
+    };
+})();
+
+var openS = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            var win = window.open('https://www.sendsonar.com/salesforce.html', '_blank');
+			 win.focus();
+        }
+    };
+})();
+
+var openC = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            var win = window.open('https://www.sendsonar.com/compliance.html', '_blank');
+			 win.focus();
+        }
+    };
+})();
